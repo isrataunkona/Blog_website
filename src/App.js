9020eaa669d5dac961454d317d0c4fd2e80c834c
Israt Irin Aunkona <96844854+isrataunkona@users.noreply.github.com>
2022-12-29 10:47:43 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Routes,Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        {/* <Route path="/" element={<Homapage></Homapage>}></Route>
+        <Route path="/alu" element={<Alu></Alu>}></Route> */}
+        
+      </Routes>
+     hello world 
+    </div>
+  );
+}
+
+export default App;
